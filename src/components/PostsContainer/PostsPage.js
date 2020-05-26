e229abb6c89086721103194023e4d7578e89b259
sam hall <sam_h_hall@icloud.com>
2020-05-26 17:36:53 -0500

    likes, posts, usernames

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,17 +1,23 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
-import Post from "./Post";
-import "./Posts.css";
-// import data 
+import React, { useState } from 'react';
+import Post from './Post';
+import './Posts.css';
+import data from '../../dummy-data';
+// import data
 
 const PostsPage = () => {
-  // set up state for your data
-  return (
-    <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
-    </div>
-  );
+    // set up state for your data
+    let [postData, setPostData] = useState(data)
+
+    return (
+        <div className='posts-container-wrapper'>
+            {/* map through data here to return a Post and pass data as props to Post */}
+            {postData.map(post => (
+                <Post post={post}/>
+            ))}
+        </div>
+    );
 };
 
 export default PostsPage;
